0a952e8f8be95569c21b281f27475a5b93763ea6
liberumChain <59084371+liberumChain@users.noreply.github.com>
2020-02-01 02:37:57 +0800

    Add files via upload

---
+++ Array+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  Array+Extension.swift
+//  chain3swift
+//
+//  Created by Alexander Vlasov on 15.01.2018.
+//  Copyright © 2018 Bankex Foundation. All rights reserved.
+//
+//  Modifications copyright © 2018 Liwei Zhang. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    /// Splits array by chunks
+    /// - parameter chunkSize: size of each subarray
+    public func split(intoChunksOf chunkSize: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: chunkSize).map {
+            let endIndex = ($0.advanced(by: chunkSize) > self.count) ? self.count - $0 : chunkSize
+            return Array(self[$0 ..< $0.advanced(by: endIndex)])
+        }
+    }
+    func safe(_ index: Int) -> Element? {
+        guard (0..<count).contains(index) else { return nil }
+        return self[index]
+    }
+}
+
